e664674aaab433ee8bed882b713d4420e6aecc58
franzmueller <franzmueller@users.noreply.github.com>
2019-03-05 07:13:49 +0100

    add option to configure number of streaming threads option can be set via environment variable "STREAM_THREADS_CONFIG" and defaults to 1 when not set (does not change current behaviour).

--- src/main/java/org/infai/seits/sepl/operators/Stream.java
+++ src/main/java/org/infai/seits/sepl/operators/Stream.java
@@ -76,6 +76,7 @@ public class Stream {
         streamsConfiguration.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
         streamsConfiguration.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
         streamsConfiguration.put(StreamsConfig.DEFAULT_TIMESTAMP_EXTRACTOR_CLASS_CONFIG, WallclockTimestampExtractor.class);
+        streamsConfiguration.put(StreamsConfig.NUM_STREAM_THREADS_CONFIG, Helper.getEnv("STREAM_THREADS_CONFIG", "1"));
 
         return streamsConfiguration;
     }
